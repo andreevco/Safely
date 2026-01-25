db8c0536de95f2185423ee4c37cfa227c084e032
Dmitrii Liulekin <liulekin@andreevco.com>
2026-01-25 20:47:20 -0300

    feat: add activity

---
+++ packages/ux/src/entities/activity/index.ts
@@ -0,0 +1,11 @@
+export { activityKeys } from './keys';
+export { useHistory } from './useHistory';
+export { fetchBtcActivity } from './api';
+export type {
+    IActivityFilters,
+    BtcActivityItem,
+    ActivityItemsDatedGroup,
+    IActivityPageParam,
+    ActivityPage,
+    IHistoryOptions
+} from './types';
