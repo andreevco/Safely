fee83a0517b12c5801f4143c4d9c84ecbc809c16
Dmitrii Liulekin <liulekin@andreevco.com>
2026-01-27 23:02:20 -0300

    chore: add icons

--- apps/mobile/src/shared/ui/Icon/icons.ts
+++ apps/mobile/src/shared/ui/Icon/icons.ts
@@ -1,13 +1,17 @@
 // Generated file. Do not edit by hand.
 
 import ChevronRight16Svg from '@safely/ux/shared/assets/icons/16/chevron-right-16.svg';
+import Sliders16Svg from '@safely/ux/shared/assets/icons/16/sliders-16.svg';
 import Xmark16Svg from '@safely/ux/shared/assets/icons/16/xmark-16.svg';
 import ArrowDown28Svg from '@safely/ux/shared/assets/icons/28/arrow-down-28.svg';
 import ArrowTop28Svg from '@safely/ux/shared/assets/icons/28/arrow-top-28.svg';
+import Checkmark28Svg from '@safely/ux/shared/assets/icons/28/checkmark-28.svg';
 import QrCodeScan28Svg from '@safely/ux/shared/assets/icons/28/qr-code-scan-28.svg';
 
+export const Sliders16 = { image: Sliders16Svg, size: 16 } as const;
 export const ChevronRight16 = { image: ChevronRight16Svg, size: 16 } as const;
 export const Xmark16 = { image: Xmark16Svg, size: 16 } as const;
 export const ArrowDown28 = { image: ArrowDown28Svg, size: 28 } as const;
 export const ArrowTop28 = { image: ArrowTop28Svg, size: 28 } as const;
 export const QrCodeScan28 = { image: QrCodeScan28Svg, size: 28 } as const;
+export const Checkmark28 = { image: Checkmark28Svg, size: 28 } as const;
