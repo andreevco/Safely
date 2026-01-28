d3d5e40db407cd9b96ce656f694abaabfb0b6421
Dmitrii Liulekin <liulekin@andreevco.com>
2026-01-27 22:54:30 -0300

    feat: setup infrastructure

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,14 +1,29 @@
+import { mmkvStorage } from '@mobile/shared/storage/mmkv';
 import { DarkTheme, Theme } from '@react-navigation/native';
 import * as SplashScreen from 'expo-splash-screen';
+import i18next from 'i18next';
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ActivityIndicator, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useUnistyles } from 'react-native-unistyles';
 
+import { AppContext, createPersister, IAppContext, QueryProvider } from '@safely/ux';
+
 import Navigation from './navigation';
+
+const persister = createPersister(mmkvStorage);
+
+const Loader = () => (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+    </View>
+);
 
 export const App = () => {
     const { theme } = useUnistyles();
+    const { t } = useTranslation();
 
     const NavigationTheme: Theme = useMemo(
         () => ({
@@ -25,10 +40,35 @@ export const App = () => {
         [theme]
     );
 
+    const appContext = useMemo<IAppContext>(
+        () => ({
+            i18n: {
+                language: i18next.language,
+                t
+            },
+            sdk: {}, // TODO: Implement IAppSdk
+            version: '1.0.0',
+            build: 'ios',
+            toast: {
+                // TODO: Implement toast service
+                show: () => {},
+                hide: () => {}
+            }
+        }),
+        [t]
+    );
+
     return (
         <GestureHandlerRootView>
             <SafeAreaProvider>
-                <Navigation onReady={() => SplashScreen.hideAsync()} theme={NavigationTheme} />
+                <QueryProvider persister={persister} loader={<Loader />}>
+                    <AppContext value={appContext}>
+                        <Navigation
+                            onReady={() => SplashScreen.hideAsync()}
+                            theme={NavigationTheme}
+                        />
+                    </AppContext>
+                </QueryProvider>
             </SafeAreaProvider>
         </GestureHandlerRootView>
     );
