5992ca129cfde9f40e4c08469cb47a849eafc7d5
Dmitrii Liulekin <liulekin@andreevco.com>
2026-02-05 23:13:30 -0300

    chore: add disable button state

--- apps/mobile/src/shared/ui/Button/Button.tsx
+++ apps/mobile/src/shared/ui/Button/Button.tsx
@@ -8,12 +8,12 @@ import { styles } from './Button.styles';
 type ButtonProps = TouchableOpacityProps & UnistylesVariants<typeof styles>;
 
 export const Button = (props: ButtonProps) => {
-    const { children, type = 'primary', size = 'medium', ...rest } = props;
+    const { children, type = 'primary', size = 'medium', disabled, style, ...rest } = props;
 
-    styles.useVariants({ type, size });
+    styles.useVariants({ type, size, disabled: disabled ? 'true' : 'false' });
 
     return (
-        <TouchableOpacity style={styles.container} {...rest}>
+        <TouchableOpacity style={[styles.container, style]} disabled={disabled} {...rest}>
             {typeof children === 'string' ? (
                 <Text variant={size === 'small' ? 'labelM' : 'labelL'}>{children}</Text>
             ) : (
