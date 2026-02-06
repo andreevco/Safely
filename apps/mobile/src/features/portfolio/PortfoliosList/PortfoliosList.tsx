0ed7f9c3e9a9c7285dc99f31840f43684490e313
Dmitrii Liulekin <liulekin@andreevco.com>
2026-02-06 10:38:28 -0300

    fix: update screens

--- apps/mobile/src/features/portfolio/PortfoliosList/PortfoliosList.tsx
+++ apps/mobile/src/features/portfolio/PortfoliosList/PortfoliosList.tsx
@@ -1,34 +1,49 @@
-import { Portfolio } from '@safely/core';
-import { useActivePortfolio } from '@safely/ux';
-
 import { PortfolioName } from '@mobile/entities/portfolio/PortfolioName/PortfolioName';
-import { Cell } from '@mobile/shared/ui';
+import { Cell, TouchableOpacity } from '@mobile/shared/ui';
 import { List } from '@mobile/shared/ui';
+
+import { Portfolio } from '@safely/core';
+import { useActivePortfolio, useSetActivePortfolio } from '@safely/ux';
 
 import { styles } from './PortfoliosList.styles';
 
 interface PortfoliosListProps {
     portfolios: Portfolio[];
+    onSelect: () => void;
 }
 
 export const PortfoliosList = (props: PortfoliosListProps) => {
-    const { portfolios } = props;
+    const { portfolios, onSelect } = props;
     const activePortfolio = useActivePortfolio();
+    const { mutate: setActivePortfolio } = useSetActivePortfolio();
+
+    const handleSelect = (portfolio: Portfolio) => {
+        if (portfolio.id.toString() === activePortfolio.id.toString()) {
+            onSelect();
+            return;
+        }
+        setActivePortfolio({ id: portfolio.id }, { onSuccess: onSelect });
+    };
 
     return (
         <List>
             <List.Group style={styles.list} variant="separated">
                 {portfolios.map(portfolio => (
-                    <Cell key={portfolio.id.toString()}>
-                        <Cell.Content>
-                            <Cell.Row>
-                                <PortfolioName meta={portfolio.meta} gap={12} size={16} />
-                            </Cell.Row>
-                        </Cell.Content>
-                        {activePortfolio.id.toString() === portfolio.id.toString() && (
-                            <Cell.Checkmark />
-                        )}
-                    </Cell>
+                    <TouchableOpacity
+                        key={portfolio.id.toString()}
+                        onPress={() => handleSelect(portfolio)}
+                    >
+                        <Cell>
+                            <Cell.Content>
+                                <Cell.Row>
+                                    <PortfolioName meta={portfolio.meta} gap={12} size={16} />
+                                </Cell.Row>
+                            </Cell.Content>
+                            {activePortfolio.id.toString() === portfolio.id.toString() && (
+                                <Cell.Checkmark />
+                            )}
+                        </Cell>
+                    </TouchableOpacity>
                 ))}
             </List.Group>
         </List>
