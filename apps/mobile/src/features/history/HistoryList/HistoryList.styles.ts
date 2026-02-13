19a489b9be8edc178f262da6530fb903212a067d
Max Voloshinskii <me@voloshinskii.com>
2026-02-13 05:33:09 +0400

    fix: add grouping by dates

--- apps/mobile/src/features/history/HistoryList/HistoryList.styles.ts
+++ apps/mobile/src/features/history/HistoryList/HistoryList.styles.ts
@@ -4,7 +4,19 @@ export const styles = StyleSheet.create(theme => ({
     contentContainer: {
         paddingHorizontal: theme.spacing[8]
     },
+    firstSectionHeader: {
+        marginTop: theme.spacing[4]
+    },
     separator: {
         height: 2
+    },
+    sectionHeaderContainer: {
+        paddingHorizontal: theme.spacing[8],
+        // compensate item separator height
+        paddingTop: 22,
+        paddingBottom: 10
+    },
+    firstSectionHeaderContainer: {
+        paddingTop: theme.spacing[16]
     }
 }));
