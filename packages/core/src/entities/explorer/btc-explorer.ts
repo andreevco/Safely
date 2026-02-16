40de6030b621921afb1e276a9e92d9862839c559
siandreev <andreev.sergey.i@yandex.ru>
2026-02-16 15:26:04 +0700

    fix: explorer url

--- packages/core/src/entities/explorer/btc-explorer.ts
+++ packages/core/src/entities/explorer/btc-explorer.ts
@@ -4,9 +4,9 @@ export class BtcExplorer {
     constructor(private readonly config: BootConfig['blockchains']['bitcoin']['mainnet']) {}
 
     public wallet(address: string): string {
-        return this.config.explorer_account_url.replace('{address}', address);
+        return this.config.explorer_account_url.replace('%s', address);
     }
     public transaction(txid: string): string {
-        return this.config.explorer_tx_url.replace('{txid}', txid);
+        return this.config.explorer_tx_url.replace('%s', txid);
     }
 }
