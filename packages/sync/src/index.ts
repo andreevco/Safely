6ebe3a68fccb0e453ef8d7b8df029854079bbc32
p0lunin <dmytro.polunin@gmail.com>
2026-02-24 18:22:26 +0200

    feature: public api in src/index.ts

---
+++ packages/sync/src/index.ts
@@ -0,0 +1,13 @@
+export type { IStorage, IEnumerableStorage, ITreeStorage } from './I-storage';
+
+export type { ISyncAccount } from './account/I-sync-account';
+export type { ISyncAccountFactory } from './account/I-sync-account-factory';
+export { SyncAccountFactory } from './account/sync-account-factory';
+
+export type { OnboardingConnector } from './onboarding/connector';
+
+export type { ISyncProvider } from './sync-provider/I-sync-provider';
+
+export { SyncError } from './sync-error';
+
+export type { ISecretEncryptor } from './secret-encryptor';
