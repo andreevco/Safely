6e277dcd0369367ab6ed0eac67a5445b4d13d75e
Dmitrii Liulekin <liulekin@andreevco.com>
2026-03-08 22:52:12 -0300

    feat: create SyncStorageProvider

---
+++ packages/ux/src/shared/providers/SyncStorageProvider.tsx
@@ -0,0 +1,42 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { FC, PropsWithChildren, useEffect } from 'react';
+
+import { useActiveAccountQuery, fiatKeys } from '../../entities';
+import { accountKey } from '../../entities/account/keys';
+import { SyncedStorageStructure, syncedStorageStructure } from '../storage';
+
+const syncedStorageKeys = Object.keys(syncedStorageStructure) as (keyof SyncedStorageStructure)[];
+
+function useSyncChangeObserver() {
+    const client = useQueryClient();
+    const { data: activeAccount } = useActiveAccountQuery();
+
+    useEffect(() => {
+        if (!activeAccount) return;
+
+        const syncProvider = activeAccount.syncProvider;
+        const accountQueryKey = accountKey.accountId(activeAccount.accountId);
+
+        const queryKeysToInvalidate: Record<keyof SyncedStorageStructure, readonly unknown[]> = {
+            portfolios: accountQueryKey.portfolios.toKey(),
+            preferredFiat: fiatKeys.active.toKey(),
+            meta: accountKey.list.toKey()
+        };
+
+        const unsubscribes = syncedStorageKeys.map(field =>
+            syncProvider.onChange(field, () => {
+                void client.invalidateQueries({ queryKey: queryKeysToInvalidate[field] });
+            })
+        );
+
+        return () => {
+            unsubscribes.forEach(fn => fn());
+        };
+    }, [activeAccount, client]);
+}
+
+export const SyncStorageProvider: FC<PropsWithChildren> = ({ children }) => {
+    useSyncChangeObserver();
+
+    return <>{children}</>;
+};
--- packages/ux/src/shared/providers/index.ts
+++ packages/ux/src/shared/providers/index.ts
@@ -1,2 +1,3 @@
 export * from './AppContext';
 export * from './QueryProvider';
+export * from './SyncStorageProvider';
