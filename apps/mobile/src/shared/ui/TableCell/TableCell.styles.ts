8cb0d908b02f16d193e82b59bb3be897b4f61929
Max Voloshinskii <me@voloshinskii.com>
2026-03-10 17:51:49 +0400

    fix(mobile): dividers

--- apps/mobile/src/shared/ui/TableCell/TableCell.styles.ts
+++ apps/mobile/src/shared/ui/TableCell/TableCell.styles.ts
@@ -1,12 +1,26 @@
 import { StyleSheet } from 'react-native-unistyles';
 
 export const styles = StyleSheet.create(theme => ({
+    touchable: {
+        backgroundColor: theme.colors.background.secondary
+    },
     container: {
         flexDirection: 'row',
         alignItems: 'center',
         gap: theme.spacing[8],
         paddingHorizontal: theme.spacing[16],
         paddingVertical: theme.spacing[12],
-        backgroundColor: theme.colors.background.secondary
+        variants: {
+            showDivider: {
+                true: {
+                    borderBottomWidth: theme.border.hairline,
+                    borderBottomColor: theme.colors.other.transparentElement
+                }
+            }
+        }
+    },
+    divider: {
+        borderBottomWidth: theme.border.hairline,
+        borderBottomColor: theme.colors.other.transparentElement
     }
 }));
