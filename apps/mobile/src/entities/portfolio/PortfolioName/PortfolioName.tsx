ec2af277a1e8f15643b644f4055389679d35769e
Max Voloshinskii <me@voloshinskii.com>
2026-03-11 18:07:34 +0400

    fix(mobile): shrink account name

--- apps/mobile/src/entities/portfolio/PortfolioName/PortfolioName.tsx
+++ apps/mobile/src/entities/portfolio/PortfolioName/PortfolioName.tsx
@@ -23,7 +23,13 @@ export const PortfolioName = (props: PortfolioNameProps) => {
                 <View style={styles.contentWithTag}>
                     <View style={styles.container(gap)}>
                         <View style={styles.dot(meta.icon.value, size)} />
-                        <Text variant={fontVariant}>{meta.name}</Text>
+                        <Text
+                            variant={fontVariant}
+                            numberOfLines={1}
+                            style={styles.name}
+                        >
+                            {meta.name}
+                        </Text>
                     </View>
                     {tag && (
                         <View style={styles.tag}>
@@ -39,7 +45,13 @@ export const PortfolioName = (props: PortfolioNameProps) => {
                 <View style={styles.contentWithTag}>
                     <View style={styles.container(gap)}>
                         <Text>{meta.icon.value}</Text>
-                        <Text variant={fontVariant}>{meta.name}</Text>
+                        <Text
+                            variant={fontVariant}
+                            numberOfLines={1}
+                            style={styles.name}
+                        >
+                            {meta.name}
+                        </Text>
                     </View>
                     {tag && (
                         <View style={styles.tag}>
