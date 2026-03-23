cbc16377ac7e6e0d09852a10e79c311eba3e7579
siandreev <andreev.sergey.i@yandex.ru>
2026-03-23 11:43:15 +0500

    fix(mobile): add delete all data button

--- apps/mobile/src/screens/SettingsScreen/SettingsScreen.tsx
+++ apps/mobile/src/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,8 +1,10 @@
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 
 import { useAppContext, useHasPortfolio } from '@safely/ux';
 
+import { useLogOutAllConfirmation } from '@mobile/features/settings/useLogOutAllConfirmation';
 import { List, Screen, Text } from '@mobile/shared/ui';
 
 import {
@@ -18,12 +20,28 @@ export const SettingsScreen = () => {
     const { t } = useTranslation();
     const { version } = useAppContext();
     const hasPortfolio = useHasPortfolio();
+    const eraseAllData = useLogOutAllConfirmation();
+    const tapCountRef = useRef(0);
 
     return (
         <Screen>
             <Screen.Header variant="center">
                 <View style={styles.headerPlaceholder} />
-                <Screen.Header.Title>{t('settings.title')}</Screen.Header.Title>
+                <Screen.Header.Title>
+                    <Pressable
+                        onPress={() => {
+                            tapCountRef.current += 1;
+                            if (tapCountRef.current >= 5) {
+                                tapCountRef.current = 0;
+                                eraseAllData();
+                            }
+                        }}
+                    >
+                        <Text textAlign="center" variant="titleS" numberOfLines={1}>
+                            {t('settings.title')}
+                        </Text>
+                    </Pressable>
+                </Screen.Header.Title>
                 <Screen.Header.CloseButton />
             </Screen.Header>
             <Screen.Scrollable contentContainerStyle={styles.container}>
