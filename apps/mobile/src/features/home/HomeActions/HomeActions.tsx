3c84165147710dc99060089f7f3240003b70b74b
Dmitrii Liulekin <liulekin@andreevco.com>
2026-04-10 19:26:30 -0300

    chore(mobile): create WATCH_ONLY_OPACITY

--- apps/mobile/src/features/home/HomeActions/HomeActions.tsx
+++ apps/mobile/src/features/home/HomeActions/HomeActions.tsx
@@ -11,6 +11,8 @@ import { ArrowDown28, ArrowTop28, QrCodeScan28 } from '@mobile/shared/ui/Icon';
 
 import { styles } from './HomeActions.styles';
 
+const WATCH_ONLY_OPACITY = 0.56;
+
 export const HomeActions = () => {
     const { t } = useTranslation();
     const navigation = useNavigation<RootStackNavigationProp<'TabsNavigator'>>();
@@ -55,7 +57,7 @@ export const HomeActions = () => {
                 title={t('home.actions.send')}
                 icon={ArrowTop28}
                 onPress={isWatchOnly ? handleWatchOnlyAction : handleNavigateToSendAsset}
-                opacity={isWatchOnly ? 0.56 : 1}
+                opacity={isWatchOnly ? WATCH_ONLY_OPACITY : 1}
             />
             <Actions.Button
                 title={t('home.actions.receive')}
@@ -66,7 +68,7 @@ export const HomeActions = () => {
                 title={t('home.actions.scan')}
                 icon={QrCodeScan28}
                 onPress={isWatchOnly ? handleWatchOnlyAction : handleQRScan}
-                opacity={isWatchOnly ? 0.56 : 1}
+                opacity={isWatchOnly ? WATCH_ONLY_OPACITY : 1}
             />
         </Actions>
     );
