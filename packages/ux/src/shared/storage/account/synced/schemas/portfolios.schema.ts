b60cac5c6641763056a06a0757213337e054c784
Dmitrii Liulekin <liulekin@andreevco.com>
2026-04-10 18:43:17 -0300

    fix(ux)!: make PortfolioType exhaustive

--- packages/ux/src/shared/storage/account/synced/schemas/portfolios.schema.ts
+++ packages/ux/src/shared/storage/account/synced/schemas/portfolios.schema.ts
@@ -1,6 +1,7 @@
 import z from 'zod';
 
 import {
+    assertUnreachable,
     PortfolioIdMnemonicBased,
     PortfolioIdWatchOnly,
     PortfolioType,
@@ -10,15 +11,19 @@ import { zArrayWithKey } from '@safely/sync';
 
 export const sPortfolios = z.union([
     zArrayWithKey(sPortfolio, item => {
-        if (item.type === PortfolioType.BIP39) {
-            return new PortfolioIdMnemonicBased(item.id.hash, item.id.networkType).toString();
+        switch (item.type) {
+            case PortfolioType.BIP39:
+                return new PortfolioIdMnemonicBased(item.id.hash, item.id.networkType).toString();
+            case PortfolioType.WATCH_ONLY:
+                return new PortfolioIdWatchOnly(
+                    item.id.identifier,
+                    item.id.source,
+                    item.id.networkType,
+                    item.id.vmType
+                ).toString();
+            default:
+                assertUnreachable(item);
         }
-        return new PortfolioIdWatchOnly(
-            item.id.identifier,
-            item.id.source,
-            item.id.networkType,
-            item.id.vmType
-        ).toString();
     }),
     z.null()
 ]);
