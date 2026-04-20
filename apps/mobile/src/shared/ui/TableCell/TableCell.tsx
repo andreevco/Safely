d83d996cf458201fc0ad2e7867935a6244c4694e
Max Voloshinskii <me@voloshinskii.com>
2026-04-20 12:18:47 +0400

    fix(mobile): correct animation

--- apps/mobile/src/shared/ui/TableCell/TableCell.tsx
+++ apps/mobile/src/shared/ui/TableCell/TableCell.tsx
@@ -30,10 +30,8 @@ export type TableCellContainerProps = {
     copyable?: string;
 };
 
-const COPIED_BACKGROUND_FADE_IN_DURATION = 180;
-const COPIED_TEXT_FADE_IN_DURATION = 200;
+const COPIED_FADE_IN_DURATION = 180;
 const COPIED_VISIBLE_DURATION = 800;
-const COPIED_FADE_OUT_DURATION = 200;
 
 export const TableCellContainer = (props: TableCellContainerProps) => {
     const { children, style, showDivider = true, copyable } = props;
@@ -45,14 +43,9 @@ export const TableCellContainer = (props: TableCellContainerProps) => {
     const theme = useUnistyles().theme;
 
     const copiedOpacity = useSharedValue(0);
-    const copiedBackgroundOpacity = useSharedValue(0);
 
     const copiedStyle = useAnimatedStyle(() => ({
         opacity: copiedOpacity.value
-    }));
-
-    const copiedBackgroundStyle = useAnimatedStyle(() => ({
-        opacity: copiedBackgroundOpacity.value
     }));
 
     const handleCopy = useCallback(() => {
@@ -60,37 +53,20 @@ export const TableCellContainer = (props: TableCellContainerProps) => {
         void setStringAsync(copyable);
         void notificationAsync(NotificationFeedbackType.Success);
 
-        copiedBackgroundOpacity.value = withSequence(
+        copiedOpacity.value = withSequence(
             withTiming(1, {
-                duration: COPIED_BACKGROUND_FADE_IN_DURATION,
+                duration: COPIED_FADE_IN_DURATION,
                 easing: Easing.in(Easing.ease)
             }),
             withDelay(
-                COPIED_TEXT_FADE_IN_DURATION + COPIED_VISIBLE_DURATION,
+                COPIED_VISIBLE_DURATION,
                 withTiming(0, {
-                    duration: COPIED_FADE_OUT_DURATION,
+                    duration: COPIED_FADE_IN_DURATION,
                     easing: Easing.out(Easing.ease)
                 })
             )
         );
-
-        copiedOpacity.value = withDelay(
-            COPIED_BACKGROUND_FADE_IN_DURATION,
-            withSequence(
-                withTiming(1, {
-                    duration: COPIED_TEXT_FADE_IN_DURATION,
-                    easing: Easing.in(Easing.ease)
-                }),
-                withDelay(
-                    COPIED_VISIBLE_DURATION,
-                    withTiming(0, {
-                        duration: COPIED_FADE_OUT_DURATION,
-                        easing: Easing.out(Easing.ease)
-                    })
-                )
-            )
-        );
-    }, [copyable, copiedOpacity, copiedBackgroundOpacity]);
+    }, [copyable, copiedOpacity]);
 
     return (
         <TouchableOpacity
@@ -101,23 +77,19 @@ export const TableCellContainer = (props: TableCellContainerProps) => {
         >
             {typeof children === 'function' ? children({ handleCopy }) : children}
             {copyable != null && (
-                <View pointerEvents="none" style={[styles.copiedIndicator]}>
-                    <Animated.View style={[styles.copiedAbsoluteContainer, copiedBackgroundStyle]}>
-                        <LinearGradient
-                            colors={[
-                                new Color(theme.colors.background.secondary).alpha(0).toString(),
-                                theme.colors.background.secondary
-                            ]}
-                            start={{ x: 0, y: 0 }}
-                            end={{ x: 1, y: 0 }}
-                            style={styles.copiedIndicatorGradient}
-                        ></LinearGradient>
-                        <View style={styles.copiedIndicatorBackground} />
-                    </Animated.View>
-                    <Animated.View style={copiedStyle}>
-                        <Text variant="labelM">{t('actions.copied')}</Text>
-                    </Animated.View>
-                </View>
+                <Animated.View pointerEvents="none" style={[styles.copiedIndicator, copiedStyle]}>
+                    <LinearGradient
+                        colors={[
+                            new Color(theme.colors.background.secondary).alpha(0).toString(),
+                            theme.colors.background.secondary
+                        ]}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 1, y: 0 }}
+                        style={styles.copiedIndicatorGradient}
+                    ></LinearGradient>
+                    <View style={styles.copiedIndicatorBackground} />
+                    <Text variant="labelM">{t('actions.copied')}</Text>
+                </Animated.View>
             )}
         </TouchableOpacity>
     );
