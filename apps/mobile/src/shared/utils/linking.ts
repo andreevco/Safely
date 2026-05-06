734b2ebedfda78630ee6442a83a3f30fc16dee75
Max Voloshinskii <me@voloshinskii.com>
2026-05-06 13:15:45 +0400

    update scheme

--- apps/mobile/src/shared/utils/linking.ts
+++ apps/mobile/src/shared/utils/linking.ts
@@ -3,7 +3,7 @@ import { Linking as RNLinking } from 'react-native';
 import { Linking } from '@safely/ux';
 
 export class MobileAppLinking extends Linking {
-    protected override authorizedOpenUrlProtocols: string[] = ['https:', 'mailto:', 'aco-swalet:'];
+    protected override authorizedOpenUrlProtocols: string[] = ['https:', 'mailto:', 'safely:'];
     protected override openWindow(url: string): void {
         void RNLinking.openURL(url);
     }
