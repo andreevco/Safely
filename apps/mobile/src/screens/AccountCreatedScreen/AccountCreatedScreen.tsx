2f4d50ba69d831947937681afe5403b72d82f270
siandreev <andreev.sergey.i@yandex.ru>
2026-05-20 19:49:33 +0200

    fix(mobile): ask password for linking device

--- apps/mobile/src/screens/AccountCreatedScreen/AccountCreatedScreen.tsx
+++ apps/mobile/src/screens/AccountCreatedScreen/AccountCreatedScreen.tsx
@@ -30,6 +30,8 @@ export const AccountCreatedScreen = () => {
         if (!activeAccount) return;
 
         using secureEncryptedStorage = getSecureEncrypted();
+
+        // don't ask for the password after initial the account is created during onboarding
         secureEncryptedStorage.UNSAFE_SKIP_SECURITY_CHECK_unlock();
 
         await connectAccountToNewDevice({ secureEncryptedStorage });
