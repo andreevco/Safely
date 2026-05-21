49bcb3a261605c291b7fb5a02ac9eb7818678635
Max Voloshinskii <me@voloshinskii.com>
2026-05-21 16:02:03 +0400

    feature(mobile): blur

---
+++ apps/mobile/src/entities/security/BlurOverlay/BlurOverlay.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { Platform, View } from 'react-native';
+import { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated';
+import { FullWindowOverlay } from 'react-native-screens';
+import { StyleSheet } from 'react-native-unistyles';
+
+import { useAppState } from '@safely/ux';
+
+import { Blur } from '@mobile/shared/ui/Blur';
+
+const OverlayComponent = Platform.OS === 'ios' ? FullWindowOverlay : View;
+
+const BLUR_INTENSITY = 100;
+const FADE_OUT_DURATION = 200;
+
+export const BlurOverlay = () => {
+    const { current } = useAppState();
+    const intensity = useSharedValue(0);
+
+    const blurAnimatedProps = useAnimatedProps(() => ({
+        intensity: intensity.value
+    }));
+
+    useEffect(() => {
+        if (['inactive', 'background'].includes(current)) {
+            intensity.value = BLUR_INTENSITY;
+        } else {
+            intensity.value = withTiming(0, { duration: FADE_OUT_DURATION });
+        }
+    }, [current, intensity]);
+
+    return (
+        <OverlayComponent pointerEvents="none" style={StyleSheet.absoluteFill}>
+            <Blur blurAnimatedProps={blurAnimatedProps} style={StyleSheet.absoluteFill} />
+        </OverlayComponent>
+    );
+};
