d20bdcdfa4e9f4d2b2a84bdb9c977cc167cd47e5
Max Voloshinskii <me@voloshinskii.com>
2026-05-21 19:07:14 +0400

    fix(mobile): unmount blur after animation

--- apps/mobile/src/entities/security/BlurOverlay/BlurOverlay.tsx
+++ apps/mobile/src/entities/security/BlurOverlay/BlurOverlay.tsx
@@ -1,7 +1,7 @@
 import * as ScreenCapture from 'expo-screen-capture';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Platform, View } from 'react-native';
-import { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated';
+import { runOnJS, useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated';
 import { FullWindowOverlay } from 'react-native-screens';
 import { StyleSheet } from 'react-native-unistyles';
 
@@ -17,6 +17,7 @@ const FADE_OUT_DURATION = 200;
 export const BlurOverlay = () => {
     const { current } = useAppState();
     const intensity = useSharedValue(0);
+    const [isVisible, setIsVisible] = useState(false);
 
     const blurAnimatedProps = useAnimatedProps(() => ({
         intensity: intensity.value
@@ -24,18 +25,27 @@ export const BlurOverlay = () => {
 
     useEffect(() => {
         if (['inactive', 'background'].includes(current)) {
+            setIsVisible(true);
             intensity.value = BLUR_INTENSITY;
             if (Platform.OS === 'android') {
                 // On Android Blur is useless for recents, because OS is showing system snapshot.
                 void ScreenCapture.preventScreenCaptureAsync();
             }
         } else {
-            intensity.value = withTiming(0, { duration: FADE_OUT_DURATION });
+            intensity.value = withTiming(0, { duration: FADE_OUT_DURATION }, finished => {
+                if (finished) {
+                    runOnJS(setIsVisible)(false);
+                }
+            });
             if (Platform.OS === 'android') {
                 void ScreenCapture.allowScreenCaptureAsync();
             }
         }
     }, [current, intensity]);
+
+    if (!isVisible) {
+        return null;
+    }
 
     return (
         <OverlayComponent pointerEvents="none" style={StyleSheet.absoluteFill}>
