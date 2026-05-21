4959ee21eb363b118b797ef2b4cfe8774ea6e39d
siandreev <andreev.sergey.i@yandex.ru>
2026-05-21 17:21:31 +0200

    Revert "fix(ux): log syncStatus"
    This reverts commit 87bae39d588569911157ca786cf74ada74adfd93.

--- packages/ux/src/entities/synced-device/index.ts
+++ packages/ux/src/entities/synced-device/index.ts
@@ -39,8 +39,6 @@ export function useAccountLinkState(): AccountLinkState {
         () => account.syncProvider.syncStatusManager.getStatus()
     );
 
-    console.log('syncStatus', account.syncProvider.syncStatusManager.getStatus());
-
     if (syncStatus === SyncStatus.DEVICE_DELETED) {
         return AccountLinkState.UNLINKED;
     }
