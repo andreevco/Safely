e5203c1b3fb92801d2b314f931e5b5acab184a9a
Max Voloshinskii <me@voloshinskii.com>
2026-05-21 19:02:16 +0400

    fix(mobile): add expo-screen-capture

--- apps/mobile/src/entities/security/BlurOverlay/BlurOverlay.tsx
+++ apps/mobile/src/entities/security/BlurOverlay/BlurOverlay.tsx
@@ -1,3 +1,4 @@
+import * as ScreenCapture from 'expo-screen-capture';
 import { useEffect } from 'react';
 import { Platform, View } from 'react-native';
 import { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated';
@@ -24,8 +25,15 @@ export const BlurOverlay = () => {
     useEffect(() => {
         if (['inactive', 'background'].includes(current)) {
             intensity.value = BLUR_INTENSITY;
+            if (Platform.OS === 'android') {
+                // On Android Blur is useless for recents, because OS is showing system snapshot.
+                void ScreenCapture.preventScreenCaptureAsync();
+            }
         } else {
             intensity.value = withTiming(0, { duration: FADE_OUT_DURATION });
+            if (Platform.OS === 'android') {
+                void ScreenCapture.allowScreenCaptureAsync();
+            }
         }
     }, [current, intensity]);
 
