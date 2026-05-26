77472a89e02c51d09d71214783fce39bad80091a
Max Voloshinskii <me@voloshinskii.com>
2026-05-26 20:45:20 +0400

    fix(mobile): replace PopupMenu with button

--- apps/mobile/src/screens/SecurityScreen/screens/AccountProtectedModal/AccountProtectedModal.tsx
+++ apps/mobile/src/screens/SecurityScreen/screens/AccountProtectedModal/AccountProtectedModal.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Pressable, View } from 'react-native';
+import { View } from 'react-native';
 
 import type { SDeviceMeta } from '@safely/sync-storage';
 import {
@@ -15,14 +15,12 @@ import { useDateFormatter } from '@safely/ux';
 import { RootStackNavigationProp } from '@mobile/app/navigation/types';
 import {
     Badge,
-    Block16,
     DeviceLinkCheckmark96,
     Icon,
-    More28,
-    PopupMenu,
     PopupMenuRef,
     Screen,
-    Text
+    Text,
+    Button
 } from '@mobile/shared/ui';
 
 import { styles } from './AccountProtectedModal.styles';
@@ -55,19 +53,9 @@ function DeviceItem(props: { ikPubHex: string; meta: SDeviceMeta; isCurrent: boo
                 </Text>
             </View>
             {!isCurrent && (
-                <PopupMenu
-                    ref={menuRef}
-                    hasBackdrop={false}
-                    variant="compact"
-                    touchable={<Icon icon={More28} color="tertiary" />}
-                >
-                    <Pressable onPress={handleDisconnect}>
-                        <View style={styles.menuItem}>
-                            <Text variant="labelL">{t('security.device.unlink')}</Text>
-                            <Icon icon={Block16} />
-                        </View>
-                    </Pressable>
-                </PopupMenu>
+                <Button type="tertiary" size="small" onPress={handleDisconnect}>
+                    {t('security.device.unlink')}
+                </Button>
             )}
         </View>
     );
