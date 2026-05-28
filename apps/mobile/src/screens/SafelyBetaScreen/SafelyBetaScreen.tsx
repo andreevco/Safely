44ee90ec270e9321a7cae19055e57c0800f1bf3c
Max Voloshinskii <me@voloshinskii.com>
2026-05-28 14:07:59 +0400

    feat(mobile): add copyable version

--- apps/mobile/src/screens/SafelyBetaScreen/SafelyBetaScreen.tsx
+++ apps/mobile/src/screens/SafelyBetaScreen/SafelyBetaScreen.tsx
@@ -6,6 +6,7 @@ import { useAppContext, useBetaFeedWatched } from '@safely/ux';
 
 import { AboutFeed } from '@mobile/features/about';
 import { Screen, Text } from '@mobile/shared/ui';
+import { useCopy } from '@mobile/shared/utils/copy';
 
 import { styles } from './SafelyBetaScreen.styles';
 
@@ -14,6 +15,7 @@ export const SafelyBetaScreen = () => {
     const { version } = useAppContext();
     const navigation = useNavigation();
     const { shouldShowBadge, markWatched } = useBetaFeedWatched();
+    const handleCopy = useCopy();
 
     useEffect(() => {
         navigation.setOptions({
@@ -33,7 +35,11 @@ export const SafelyBetaScreen = () => {
             <Screen.Header variant="left">
                 <Screen.Header.Title>
                     <Text variant="titleS">{t('safelyBeta.title')}</Text>
-                    <Text variant="bodyM" color="secondary">
+                    <Text
+                        onPress={() => handleCopy(version, t('safelyBeta.versionCopied'))}
+                        variant="bodyM"
+                        color="secondary"
+                    >
                         {t('safelyBeta.subtitle', { version })}
                     </Text>
                 </Screen.Header.Title>
