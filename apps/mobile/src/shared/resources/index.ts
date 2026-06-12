c2bf9d3d6133b0bb1349db00cc680a5070ca4333
Dmitrii Liulekin <liulekin@andreevco.com>
2026-06-12 19:21:26 -0300

    feat(chore): add some resources

--- apps/mobile/src/shared/resources/index.ts
+++ apps/mobile/src/shared/resources/index.ts
@@ -1,3 +1,4 @@
+import LedgerPreview from './images/ledger-preview.png';
 import SafelyLogoWithBg from './images/safely-logo-with-bg.png';
 import SafelyLogo from './images/safely-logo.svg';
 import SlidersBoxed from './images/sliders-boxed.png';
@@ -7,5 +8,6 @@ export const resources = {
     safelyLogo: SafelyLogo,
     safelyLogoWithBg: SafelyLogoWithBg,
     slidersBoxed: SlidersBoxed,
+    ledgerPreview: LedgerPreview,
     welcomeScreenBg: WelcomeScreenBg
 } as const;
