a133eee01eea9944565292e05b923853ea689e5b
Dmitrii Liulekin <liulekin@andreevco.com>
2026-06-20 13:28:54 -0300

    fix(mobile): get back the ble verification logic

--- apps/mobile/src/features/ledger/getBluetoothState.ts
+++ apps/mobile/src/features/ledger/getBluetoothState.ts
@@ -1,17 +1,16 @@
-import { State } from 'react-native-ble-plx';
+import { BleManager, State } from 'react-native-ble-plx';
 
 export const getBluetoothState = (): Promise<State> =>
     new Promise(resolve => {
-        resolve(State.PoweredOn);
-        // const manager = new BleManager();
+        const manager = new BleManager();
 
-        // const subscription = manager.onStateChange(state => {
-        //     if (state === State.Unknown || state === State.Resetting) {
-        //         return;
-        //     }
-        //
-        //     subscription.remove();
-        //     manager.destroy();
-        //     resolve(state);
-        // }, true);
+        const subscription = manager.onStateChange(state => {
+            if (state === State.Unknown || state === State.Resetting) {
+                return;
+            }
+
+            subscription.remove();
+            manager.destroy();
+            resolve(state);
+        }, true);
     });
