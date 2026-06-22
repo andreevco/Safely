01fb790b82f1a32e466943a28f8465350cd0ff8d
siandreev <andreev.sergey.i@yandex.ru>
2026-06-22 17:38:46 +0200

    fix: set btc rate to 0 for testnet

--- packages/ux/src/entities/asset/useRate.ts
+++ packages/ux/src/entities/asset/useRate.ts
@@ -1,19 +1,26 @@
 import type { CryptoAsset, CryptoFiatRate } from '@safely/core';
+import { toBig } from '@safely/core';
+import { Rate } from '@safely/core';
 
 import { assetKeys } from './keys';
 import { getRateFn } from './rateQuery';
 import { usePersistQuery, usePriceApi } from '../../shared';
 import { useActiveFiat } from '../fiat/useActiveFiat';
+import { useIsActivePortfolioTestnet } from '../portfolio';
 
 export function useRate(asset: CryptoAsset) {
     const fiat = useActiveFiat();
     const priceApi = usePriceApi();
+    const isTestnet = useIsActivePortfolioTestnet();
 
-    const query = usePersistQuery<CryptoFiatRate | null>({
-        queryKey: assetKeys.rate(asset.id.toString()).fiat(fiat.id.toString()).toKey(),
-        queryFn: () => getRateFn(priceApi, asset, fiat),
+    return usePersistQuery<CryptoFiatRate | null>({
+        queryKey: assetKeys
+            .rate(asset.id.toString())
+            .fiat(fiat.id.toString())
+            .params({ isTestnet })
+            .toKey(),
+        queryFn: () =>
+            isTestnet ? new Rate(asset, fiat, toBig(0)) : getRateFn(priceApi, asset, fiat),
         schemaKey: 'sCryptoFiatRate'
     });
-
-    return query;
 }
