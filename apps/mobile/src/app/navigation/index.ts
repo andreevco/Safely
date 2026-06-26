bf07d07715271aa848bce2d13b5cbe23105511c4
Max Voloshinskii <me@voloshinskii.com>
2026-06-26 12:33:57 +0400

    fix(mobile): correct reconnect flow

--- apps/mobile/src/app/navigation/index.ts
+++ apps/mobile/src/app/navigation/index.ts
@@ -20,6 +20,7 @@ import { PasscodeVerificationScreen } from '@mobile/screens/PasscodeVerification
 import { PendingFundsSheet } from '@mobile/screens/PendingFundsSheet';
 import { QRScanModal } from '@mobile/screens/QRScanModal';
 import { ReceiveAssetModal } from '@mobile/screens/ReceiveAssetModal';
+import { ReconnectDeviceModal } from '@mobile/screens/ReconnectDeviceModal';
 import { RemoveWalletSheet } from '@mobile/screens/RemoveWalletSheet';
 import {
     DisconnectDeviceSheet,
@@ -105,6 +106,7 @@ export const RootStack = createNativeStackNavigator({
                 SelectAccountModal: SelectAccountModal,
                 SelectAccountSelectorModal: SelectAccountSelectorModal,
                 ReceiveAssetModal: ReceiveAssetModal,
+                ReconnectDeviceModal: ReconnectDeviceModal,
                 SendAssetModal: SendStack
             },
             screenOptions: {
--- apps/mobile/src/features/device-link/DeviceUnlinkedBanner/DeviceUnlinkedBanner.tsx
+++ apps/mobile/src/features/device-link/DeviceUnlinkedBanner/DeviceUnlinkedBanner.tsx
@@ -19,7 +19,7 @@ export const DeviceUnlinkedBanner = ({ style }: DeviceUnlinkedBannerProps) => {
     const linkState = useAccountLinkState();
 
     const handlePress = useCallback(() => {
-        navigation.navigate('TabsNavigator', { screen: 'SafetyScreen' });
+        navigation.navigate('ReconnectDeviceModal');
     }, [navigation]);
 
     if (linkState !== AccountLinkState.UNLINKED) {
