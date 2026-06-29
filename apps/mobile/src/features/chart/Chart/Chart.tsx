3aad615de0fe7730460c6703c31adb7fea5dca67
Max Voloshinskii <me@voloshinskii.com>
2026-06-29 15:23:01 +0400

    chore(mobile): make attribution required

--- apps/mobile/src/features/chart/Chart/Chart.tsx
+++ apps/mobile/src/features/chart/Chart/Chart.tsx
@@ -67,10 +67,10 @@ export const Chart = () => {
                 formattedTime={crosshair.formattedTime}
                 secondaryCrosshair={crosshair.secondaryCrosshair}
             />
-            {chart.data?.attribution?.label && (
+            {chart.data?.attribution.label && (
                 <ProviderLabel
-                    label={chart.data?.attribution?.label}
-                    url={chart.data?.attribution?.url}
+                    label={chart.data?.attribution.label}
+                    url={chart.data?.attribution.url}
                 />
             )}
         </View>
