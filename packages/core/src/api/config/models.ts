cc5da245d792a1c5f00ec23a7f9e0b330c69f033
Dmitrii Liulekin <liulekin@andreevco.com>
2026-07-23 17:27:03 -0300

    fix(core): update flagsSchema

--- packages/core/src/api/config/models.ts
+++ packages/core/src/api/config/models.ts
@@ -13,7 +13,8 @@ export interface ConfigParams {
 // GET /config
 
 export const flagsSchema = z.looseObject({
-    enable_onramps: z.boolean().default(false)
+    enable_onramps: z.boolean().default(false),
+    enable_app_restrictions: z.boolean().default(false)
 });
 
 export type Flags = z.infer<typeof flagsSchema>;
