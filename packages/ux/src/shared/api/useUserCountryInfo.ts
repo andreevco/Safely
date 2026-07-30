987240369fee966c4c5178d5c831d7dedf47faaf
Dmitrii Liulekin <liulekin@andreevco.com>
2026-07-30 01:12:59 -0300

    fix(ux): change endless staleTime for country info

--- packages/ux/src/shared/api/useUserCountryInfo.ts
+++ packages/ux/src/shared/api/useUserCountryInfo.ts
@@ -2,7 +2,7 @@ import type { UserCountryInfo } from '@safely/core';
 
 import { apiKeys } from './keys';
 import { useAppContext } from '../providers';
-import { usePersistSuspenseQuery } from '../query-core';
+import { QUERIES_STALE_TIME, usePersistSuspenseQuery } from '../query-core';
 
 export function useUserCountryInfo(): UserCountryInfo {
     const { getUserCountryInfo } = useAppContext();
@@ -10,6 +10,7 @@ export function useUserCountryInfo(): UserCountryInfo {
     return usePersistSuspenseQuery<UserCountryInfo>({
         queryKey: apiKeys.userCountryInfo().toKey(),
         queryFn: () => getUserCountryInfo(),
+        staleTime: query => (query.state.data?.storeCode === null ? 0 : QUERIES_STALE_TIME.DEFAULT),
         schemaKey: 'userCountryInfo'
     }).data;
 }
